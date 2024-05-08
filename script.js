cf95d5b9bab3dee93c4e40dfbb0a448ca018e9cb
suvadityagupta <10suvaditya18@gmail.com>
2024-05-08 23:05:32 +0530

    Updated API key in script.js
    Explanation:
    - The variable API\_KEY has been updated with a new value "71815003bdca4f38be6301584

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "71815003bdca4f38be63015841c182f4";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
