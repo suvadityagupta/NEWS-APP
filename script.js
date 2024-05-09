65aa72725c3791fd09ee8e5df8989fe240c2db2a
Suvaditya Gupta <10suvaditya18@gmail.com>
2024-05-09 15:42:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const API_KEY = "71815003bdca4f38be63015841c182f4";
 const url = "https://newsapi.org/v2/everything?q=";
-
 window.addEventListener("load", () => fetchNews("India"));
 
 function reload() {
